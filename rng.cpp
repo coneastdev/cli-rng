035c6eb95e7fdca623ceff9c9e90134e5d968d1b
coneastdev <coneastdev@proton.me>
2026-09-18 16:38:21 +0100

    add debug command

--- rng.cpp
+++ rng.cpp
@@ -8,20 +8,30 @@ void help() {
     std::cout << "(coin flip)\nrng --c / rng -c [number] [number]\nRuns a coin flip, by default it is 50-50 but you can add the probability ratio\n(dice roll)\nrng --d [number] [number]\nRuns a dice roll, you can specify the number of faces the die has, you can also specify the amount of dice to roll.\nIf the face number is left blank it will default to 6, if dice count is blank it will default to 1\n";
 }
 
-std::string coin(int argc, char* argv[], std::mt19937 gen) {
+std::string coin(int argc, char* argv[], std::mt19937 gen, bool debug) {
     double probability;
-    if (argc == 2) {
+    if (argc == 2 || (argc == 3 && debug)) {
         probability = 0.5;
     } else if (argc == 4) {
         double a = std::stoi(argv[2]);
         double b = std::stoi(argv[3]);
         double probabilityOfHeads = (a / (a + b));
         probability = probabilityOfHeads;
+    } else if (argc == 5 && debug) {
+        double a = std::stoi(argv[3]);
+        double b = std::stoi(argv[4]);
+        double probabilityOfHeads = (a / (a + b));
+        probability = probabilityOfHeads;
     }
+    
     
     
     std::bernoulli_distribution coin(probability);
     bool result = coin(gen);
+
+    if (debug) {
+        std::cout << ("probability of heads: " + std::to_string(probability) + "\n");
+    }
 
     if (result) {
         return "Heads";
@@ -30,26 +40,35 @@ std::string coin(int argc, char* argv[], std::mt19937 gen) {
     }
 }
 
-std::string dice(int argc, char* argv[], std::mt19937 gen) {
+std::string dice(int argc, char* argv[], std::mt19937 gen, bool debug) {
     std::string result = "";
     int faces = 6;
 
-    if (argc > 2) {
+    // check if faces is specified
+    if (argc > 3 && debug) {
+        faces = std::stoi(argv[3]);
+    } else if (argc > 2 && !debug) {
         faces = std::stoi(argv[2]);
     }
+    
 
     std::uniform_int_distribution<int> dice(1, faces);
 
     int nums = 1;
 
-    
-
-    if (argc > 3) {
+    // check if dice count is specified
+    if (argc > 4 && debug) {
+        nums = std::stoi(argv[4]);
+    } else if (argc > 3 && !debug) {
         nums = std::stoi(argv[3]);
     }
-
+    
     for (int i = 0; i < nums; i++) {
         result += (std::to_string(dice(gen)) + " ");
+    }
+
+    if (debug) {
+        std::cout << ("faces: " + std::to_string(faces) + ", dices: " + std::to_string(nums) + "\n");
     }
 
     return result;
@@ -60,23 +79,31 @@ int main(int argc, char* argv[]) {
         {"help", no_argument, nullptr, 'h'},
         {"coin", optional_argument, nullptr, 'c'},
         {"dice", optional_argument, nullptr, 'd'},
+        {"show", no_argument, nullptr, 's'},
         {nullptr, 0, nullptr, 0}
     };
 
     std::random_device rd;
     std::mt19937 gen(rd());
 
+    bool debug = false;
+
     int opt;
     while ((opt = getopt_long(argc, argv, "hc:d:", long_options, nullptr)) != -1) {
         switch (opt) {
+            case 's':
+                debug = true;
+                break;
             case 'h':
                 help();
                 break;
             case 'c':
-                std::cout << coin(argc, argv, gen);
+                std::cout << coin(argc, argv, gen, debug);
+                std::cout << "\n";
                 break;
             case 'd':
-                std::cout << dice(argc, argv, gen);
+                std::cout << dice(argc, argv, gen, debug);
+                std::cout << "\n";
                 break;
             default:
                 break;
@@ -86,8 +113,6 @@ int main(int argc, char* argv[]) {
     if (argc < 2) {
         help();
     }
-
-    std::cout << "\n";
 
     return 0;
 }
