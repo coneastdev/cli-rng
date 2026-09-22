64b8012df3f05623bd1e71914c020b0abc780f5d
coneastdev <coneastdev@proton.me>
2026-09-22 22:27:43 +0100

    update help command

--- rng.cpp
+++ rng.cpp
@@ -5,7 +5,7 @@
 
 // help, shows commands, how to run them and what they do
 void help() {
-    std::cout << "(show)\nrng --s / rng --s [command]\nDisplays debug information\n\n(coin flip)\nrng --c / rng -c [number] [number]\nRuns a coin flip, by default it is 50-50 but you can add the probability ratio\n\n(dice roll)\nrng --d [number] [number]\nRuns a dice roll, you can specify the number of faces the die has, you can also specify the amount of dice to roll.\nIf the face number is left blank it will default to 6, if dice count is blank it will default to 1\n";
+    std::cout << "show debug info\n--s / --show [next command]\nUse before a command to show the debug information\n\ncoin flip\n--c / --coin [number] [number]\nRuns a coin flip, by default it is 50-50 but you can add the probability ratio\n\ndice roll\n--d / --dice [number] [number]\nRuns a dice roll, you can specify the faces on the dice and the amount of dice, by default it is one d6";
 }
 
 // coin, returns heads or tails based on probability and input
